5243f78a0dc1ce8cea5e08117022b40d0fe4e7bf
fsdtrinings <javafsd.trainings@gmail.com>
2025-10-13 13:03:12 +0530

    adding few more parameterized test cases

--- FinOpsSpringApp/src/test/java/com/mkj/app/service/hrservice/DummyServiceTest.java
+++ FinOpsSpringApp/src/test/java/com/mkj/app/service/hrservice/DummyServiceTest.java
@@ -2,10 +2,16 @@ package com.mkj.app.service.hrservice;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.context.annotation.Description;
 
@@ -106,13 +112,11 @@ class DummyServiceTest {
 	
 	}
 	
-
+	// Note : there is no @Test annotation
 	@ParameterizedTest
 	@ValueSource(ints = {-8500,0,10000,18000})
-	//@Test // 1st Issue
 	@Description("this test will throw a IllegalArg Exception if Employee salary is less than 0 or greater than 10000")
-
-	//@Disabled
+	@Disabled
 	void testGetSalaryByUser_EmployeeSalaryIfSalaryOutofbound(int salary)
 	{
 		// Sample input 
@@ -128,6 +132,66 @@ class DummyServiceTest {
 		assertEquals("Employee Salary is outofbound", e.getMessage());
 		
 	}
+	
+	
+	
+	@ParameterizedTest
+	@MethodSource("getAllSampleEmployee") 
+	@Description("SameTest But through method source_this test will throw a IllegalArg Exception if Employee salary is less than 0 or greater than 10000")
+	@Disabled
+	void testGetSalaryByUser_EmployeeSalaryIfSalaryOutofbound2(Employee emp)
+	{
+		// Sample input 
+		Employee sampleEmployee = new Employee();
+		sampleEmployee.setEmpCode(101);
+		sampleEmployee.setSalary(emp.getSalary());
+		
+		IllegalArgumentException e = assertThrows
+				(IllegalArgumentException.class, () -> service.getSalaryByUser(sampleEmployee));
+		    /// checking different statement
+		//assertEquals("Employee Salary is less than 0", e.getMessage());
+		
+		assertEquals("Employee Salary is outofbound", e.getMessage());
+		
+	}
+	
+	public static List<Employee> getAllSampleEmployee()
+	{
+	
+		List<Employee> list=new ArrayList<>();
+		list.add(new Employee(101, null, null, null, -8500, null, null, null));
+		list.add(new Employee(102, null, null, null, 0, null, null, null));
+		list.add(new Employee(103, null, null, null, 10500, null, null, null));
+		list.add(new Employee(104, null, null, null, 10000, null, null, null));
+		
+		
+		return list;
+	}
+	
+	
+	@ParameterizedTest
+	@CsvSource({
+	    "101, A, -8500",
+	    "102, B, 0",
+	    "103, C, 10000",
+	    "104, D, 18000"
+	})
+	//@Disabled
+	void testGetSalaryByUser_EmployeeSalaryOutOfBound(int empCode, String name, int salary) {
+	    Employee emp = new Employee();
+	    emp.setEmpCode(empCode);
+	    emp.setEmpName(name);
+	    emp.setSalary(salary);
+
+	    IllegalArgumentException e = assertThrows
+				(IllegalArgumentException.class, () -> service.getSalaryByUser(emp));
+		
+
+	    assertEquals("Employee Salary is outofbound", e.getMessage());
+	}
+	
+	
+	
 	
 }//end test class
 
